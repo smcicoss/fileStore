677f4881d8ff8573eb26687f8e430562a30985ec
smcicoss <simon@cicoss.net>
2020-02-10 17:08:04 +0100

    Prueba de librerías

--- scripts/libStore/bdStore.py
+++ scripts/libStore/bdStore.py
@@ -10,5 +10,24 @@ Módulo librería para el acceso a la base de datos de
 fileStore
 """
 
-from libStore import constStore as cons
+import pymysql
+
+# from libStore import constStore as cons
 from libStore import configStore as cnf
+
+db = pymysql.connect(cnf.conf["dbHost"], cnf.conf["dbUser"],
+                     cnf.conf["dbPasswd"], cnf.conf["baseDatos"])
+
+# prepara un objeto cursor usando el metodo  cursor()
+cursor = db.cursor()
+
+# ejecuta el SQL query usando el metodo execute().
+cursor.execute("SELECT VERSION()")
+
+# procesa una unica linea usando el metodo fetchone().
+version = cursor.fetchone()
+
+
+def close():
+    # desconecta del servidor
+    db.close()
--- scripts/prueba.py
+++ scripts/prueba.py
@@ -12,8 +12,12 @@ fileStore
 
 from libStore import constStore as cons
 from libStore import configStore as cnf
-from libStore import bdStore as bd
+from libStore import bdStore as bdcnf
 
 if __name__ == '__main__':
     print(cons.MSG_OK + "Script de pruebas de fileStore." + cons.MSG_END)
-    print(cnf.conf)
+    print("versión gestor Base de Datos:%s %s %s" %
+          (cons.MSG_OK, bdcnf.version[0], cons.MSG_END))
+    bdcnf.close()
+    print("Path al almacén: %s%s%s" %
+          (cons.MSG_OK, cnf.conf["storePath"], cons.MSG_END))
